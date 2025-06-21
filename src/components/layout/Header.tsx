49f98e40bf157bce5ead86d1cf4e9fbcf6ca17e7
matin aliakbari <matin54843590@gmail.com>
2025-06-21 20:55:22 +0330

    feat: Add global styles, layout, and header components for the Next.js ecommerce application
    - Implemented global CSS styles including Bootstrap and custom styles.
    - Created a root layout component to wrap the application with metadata and header.
    - Developed a header component with navigation links and a responsive design.
    - Added Bootstrap client component to handle Bootstrap JavaScript.
    - Initialized the main page structure with a placeholder for content.
    - Configured TypeScript settings in tsconfig.json for the project.

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,168 @@
+"use client"
+
+import Image from "next/image";
+import HeroImage from '../../../public/images/hero-bg.jpg';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
+export default function Header() {
+
+    const pathname = usePathname()
+
+    return (
+        <div className={pathname === '/' ? '' : 'sub_page'}>
+            <div className="hero_area">
+                <div className="bg-box">
+                    <Image src={HeroImage} priority alt="hero-image" />
+                </div>
+
+                <header className="header_section">
+                    <div className="container">
+                        <nav className="navbar navbar-expand-lg custom_nav-container">
+                            <Link className="navbar-brand" href="/">
+                                <span>
+                                    فروشگاه فودی
+                                </span>
+                            </Link>
+
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav mx-auto">
+                                    <li className={pathname === '/' ? "nav-item active" : "nav-item"}>
+                                        <Link className="nav-link" href="/">صفحه اصلی</Link>
+                                    </li>
+                                    <li className={pathname === '/menu' ? "nav-item active" : "nav-item"}>
+                                        <Link className="nav-link" href="/menu">منو</Link>
+                                    </li>
+                                    <li className={pathname === '/about' ? "nav-item active" : "nav-item"}>
+                                        <Link className="nav-link" href="/about">درباره ما</Link>
+                                    </li>
+                                    <li className={pathname === '/contact' ? "nav-item active" : "nav-item"}>
+                                        <Link className="nav-link" href="/contact">تماس با ما</Link>
+                                    </li>
+                                </ul>
+                                <div className="user_option">
+                                    <Link className="cart_link position-relative" href="/cart">
+                                        <i className="bi bi-cart-fill text-white fs-5"></i>
+                                        <span className="position-absolute top-0 translate-middle badge rounded-pill">
+                                            3
+                                        </span>
+                                    </Link>
+
+                                    <Link href="/auth/login" className="btn-auth">
+                                        ورود
+                                    </Link>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </header>
+
+                {pathname === '/' && (
+                    <section className="slider_section">
+                        <div id="customCarousel1" className="carousel slide" data-bs-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-md-7 col-lg-6">
+                                                <div className="detail-box">
+                                                    <h2 className="mb-3 fw-bold">
+                                                        لورم ایپسوم متن ساختگی
+                                                    </h2>
+                                                    <p>
+                                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+                                                        از
+                                                        طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                                                        سطرآنچنان که
+                                                        لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
+                                                        بهبود
+                                                        ابزارهای کاربردی می باشد.
+                                                    </p>
+                                                    <div className="btn-box">
+                                                        <Link href="/menu" className="btn1">
+                                                            سفارش
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-md-7 col-lg-6">
+                                                <div className="detail-box">
+                                                    <h2 className="mb-3 fw-bold">
+                                                        لورم ایپسوم متن ساختگی
+                                                    </h2>
+                                                    <p>
+                                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+                                                        از
+                                                        طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                                                        سطرآنچنان که
+                                                        لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
+                                                        بهبود
+                                                        ابزارهای کاربردی می باشد.
+                                                    </p>
+                                                    <div className="btn-box">
+                                                        <Link href="/menu" className="btn1">
+                                                            سفارش
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-md-7 col-lg-6">
+                                                <div className="detail-box">
+                                                    <h2 className="mb-3 fw-bold">
+                                                        لورم ایپسوم متن ساختگی
+                                                    </h2>
+                                                    <p>
+                                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+                                                        از
+                                                        طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+                                                        سطرآنچنان که
+                                                        لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف
+                                                        بهبود
+                                                        ابزارهای کاربردی می باشد.
+                                                    </p>
+                                                    <div className="btn-box">
+                                                        <Link href="/menu" className="btn1">
+                                                            سفارش
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="container">
+                                <ol className="carousel-indicators">
+                                    <li data-bs-target="#customCarousel1" data-bs-slide-to="0" className="active"></li>
+                                    <li data-bs-target="#customCarousel1" data-bs-slide-to="1"></li>
+                                    <li data-bs-target="#customCarousel1" data-bs-slide-to="2"></li>
+                                </ol>
+                            </div>
+                        </div>
+                    </section>
+                )
+                }
+
+            </div>
+        </div>
+    );
+}
---
+++ src/components/lib/BootstrapClient.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function BootstrapClient() {
+
+    useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-require-imports
+        require("bootstrap/dist/js/bootstrap.bundle.js")
+    }, [])
+
+    return null;
+}
