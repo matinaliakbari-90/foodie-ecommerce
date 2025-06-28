24c6f04a4fa40e4759f6f223230ccf49afc84f3e
matin aliakbari <matin54843590@gmail.com>
2025-06-28 21:21:03 +0330

    add redux-persist for state persistence; update store configuration and wrap Provider with PersistGate

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { cartReducer } from './slices/cartSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, cartReducer)
 
 export const store = configureStore({
     reducer: {
-        shoppingCart: cartReducer
+        shoppingCart: persistedReducer
     },
+
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware({
+            serializableCheck: false
+        })
+    }
 })
+
+export const persistor = persistStore(store)
 
 
 export type RootState = ReturnType<typeof store.getState>
